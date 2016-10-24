dfd2f4c567c617e5d47ef965befe10b7ae4c3218
Justin Yang <zake7749@gmail.com>
2016-10-25 00:17:09 +0800

    Remove idle messages.

--- segment.py
+++ segment.py
@@ -9,15 +9,15 @@ def main():
 
     # jieba custom setting.
     jieba.set_dictionary('jieba_dict/dict.txt.big')
+    
     # load stopwords set
     stopwordset = set()
     with open('jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
         for line in sw:
             stopwordset.add(line.strip('\n'))
-
-    for word in stopwordset:
-        print(word)
-
+    
+    texts_num = 0
+    
     output = open('wiki_seg.txt','w')
     with open('wiki_zh_tw.txt','r') as content :
         for line in content:
@@ -32,4 +32,4 @@ def main():
     output.close()
 
 if __name__ == '__main__':
-    main()
+	main()
