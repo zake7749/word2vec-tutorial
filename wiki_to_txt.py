bce916c206f43625d06b4f072c53bfb4d729d57e
Justin Yang <zake7749@gmail.com>
2017-06-24 21:17:01 +0800

    Update for gensim v2.2.0

--- wiki_to_txt.py
+++ wiki_to_txt.py
@@ -16,7 +16,7 @@ def main():
 
     with open("wiki_texts.txt",'w',encoding='utf-8') as output:
         for text in wiki_corpus.get_texts():
-            output.write(b' '.join(text).decode('utf-8') + '\n')
+            output.write(' '.join(text) + '\n')
             texts_num += 1
             if texts_num % 10000 == 0:
                 logging.info("已處理 %d 篇文章" % texts_num)
