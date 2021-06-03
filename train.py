b3f516b91d118824bb90f834184aa25a5a5f1c68
Justin Yang <zake7749@gmail.com>
2021-06-03 12:36:47 +0800

    [U] Fix the argument name to adapt gensim 4.0.

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ def main():
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     sentences = word2vec.LineSentence("wiki_seg.txt")
-    model = word2vec.Word2Vec(sentences, size=250)
+    model = word2vec.Word2Vec(sentences, vector_size=250)
 
     #保存模型，供日後使用
     model.save("word2vec.model")
